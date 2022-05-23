726b3bc1f21c7e8b088f48ac7e0507341109f615
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-05-23 11:57:18 +0200

    Refactor

--- laravel/resources/views/home/index.blade.php
+++ laravel/resources/views/home/index.blade.php
@@ -7,14 +7,14 @@
 @section('content')
     <section class="home">
         <div class="home__text-wrapper">
-            <div class="home__main-text">Bet thousands of
+            <h1 class="home__main-text">Bet thousands of
                 <span class="home__main-text-green">
                     free valuebets
                 </span>
-            </div>
-            <div class="home__desc">
+            </h1>
+            <p class="home__desc">
                 Use free valuebets finder and tools for making money on betting.
-            </div>
+            </p>
             <div class="home__button">
                 <a href="/valuebets">
                     <i class="fa-solid fa-magnifying-glass"></i>
